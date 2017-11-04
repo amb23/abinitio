905b636a814b6768ab267d6e3204dc04e922c4dc
Adam Biggs <ambiggs23@gmail.com>
2017-11-04 19:25:17 +0000

    Refactor
    Moved all the algo generic work out of stats into functional

---
+++ abinitio/functional.py
@@ -0,0 +1,159 @@
+"""
+This module builds out some of the core functional tools we use throughout the
+rest of the repo
+"""
+
+import numpy as np
+
+
+class IStoppingCondition(object):
+    """
+    IStoppingCondition
+
+    This class is used for determining when a stopping condition has been
+    reached.    Implementations can be found below
+    """
+
+    def handle_update(self, value):
+        """
+
+        Parameters
+        ----------
+        value: numpy.array
+            Most recent value created from the algorithm
+
+        Returns
+        -------
+
+        """
+        raise NotImplementedError
+
+    def should_stop(self):
+        """
+
+        Returns
+        -------
+        bool
+            Whether the condition has been satisfied
+        """
+        raise NotImplementedError
+
+
+class IterationStoppingCondition(IStoppingCondition):
+    """
+    IterationStoppingCondition
+
+    Stopping condition is satisfied when a certain number of iterations have
+    reached
+    """
+
+    def __init__(self, max_iterations=100):
+        self._max_iterations = max_iterations
+        self._iterations = 0
+
+    def handle_update(self, value):
+        self._iterations += 1
+
+    def should_stop(self):
+        return self._iterations > self._max_iterations
+
+
+class DifferenceStoppingCondition(IStoppingCondition):
+    """
+    DifferenceStoppingCondition
+
+    Stopping condition is satisfied when the difference between the current
+    value and the last is below a threshold
+    """
+
+    def __init__(self, min_difference=1e-5):
+        self._min_difference = min_difference
+        self._last_value = None
+        self._difference = None
+
+    def handle_update(self, value):
+        if self._last_value is not None:
+            diff = value - self._last_value
+            self._difference = np.sqrt(np.dot(diff, diff))
+        self._last_value = value
+
+    def should_stop(self):
+        if self._difference is not None:
+            return self._difference < self._min_difference
+        return False
+
+
+class MultiStoppingCondition(IStoppingCondition):
+    """
+    MultiStoppingCondition
+
+    Contains a selection of stopping conditions. It can be constructed so that
+    either any or all of them should have stopped for this condition to have
+    been reached
+    """
+
+    def __init__(self, conditions, when='all'):
+        """
+
+        Parameters
+        ----------
+        conditions: list<IStoppingCondition>
+            The sub conditions that go into this multi condition
+        when: {'all', 'any'}
+            If 'all' then all sub conditions must be satisfied for the stop,
+            if 'any' then only one must be.
+        """
+        self._conditions = conditions
+        self._when = when
+
+    def handle_update(self, value):
+        for condition in self._conditions:
+            condition.handle_update(value)
+
+    def should_stop(self):
+        return getattr(np, self._when)(
+            [
+                condition.should_stop() for condition in self._conditions
+            ]
+        )
+
+
+class StoppingCondition(MultiStoppingCondition):
+    """
+    Default stopping condition stops after a number of iterations or if the
+    difference is suitably small
+    """
+
+    def __init__(self, max_iterations=1000, min_difference=1e-5):
+        super(StoppingCondition, self).__init__(
+            [
+                IterationStoppingCondition(max_iterations=max_iterations),
+                DifferenceStoppingCondition(min_difference=min_difference)
+            ],
+            'any'
+        )
+
+
+def iteration_algorithm(update, initial_condition, stopping_condition=None):
+    """
+
+    Parameters
+    ----------
+    update: Callable<numpy.array(numpy.array)>
+    initial_condition: numpy.array
+    stopping_condition: IStoppingCondition or None
+        defaults to StoppingCondition
+    Returns
+    -------
+    numpy.array
+        The result whn the stopping condition has been met
+    """
+    stopping_condition = stopping_condition or StoppingCondition()
+
+    state = initial_condition
+
+    while not stopping_condition.should_stop():
+        state = update(state)
+        stopping_condition.handle_update(state)
+
+    return state
--- abinitio/stats.py
+++ abinitio/stats.py
@@ -2,128 +2,54 @@
 import numpy as np
 import pandas as pd
 
-
-class IStoppingCondition(object):
-
-    def handle_update(self, value):
-        raise NotImplementedError
-
-    def should_stop(self):
-        raise NotImplementedError
-
-
-class IterationStoppingCondition(IStoppingCondition):
-
-    def __init__(self, max_iterations=100, **kwargs):
-        super(IterationStoppingCondition, self).__init__(**kwargs)
-        self._max_iterations = max_iterations
-        self._iterations = 0
-
-    def handle_update(self, value):
-        self._iterations += 1
-
-    def should_stop(self):
-        return self._iterations > self._max_iterations
-
-
-class DifferenceStoppingCondition(IStoppingCondition):
-    def __init__(self, min_difference=1e-5, **kwargs):
-        super(DifferenceStoppingCondition, self).__init__(**kwargs)
-        self._min_difference = min_difference
-        self._last_value = None
-        self._difference = None
-
-    def handle_update(self, value):
-        if self._last_value is not None:
-            diff = value - self._last_value
-            self._difference = np.sqrt(np.dot(diff, diff))
-        self._last_value = value
-
-    def should_stop(self):
-        if self._difference is not None:
-            return self._difference < self._min_difference
-        return False
-
-
-class History(IterationStoppingCondition):
-
-    def __init__(self, **kwargs):
-        super(History, self).__init__(**kwargs)
-        self._history = []
-
-    @property
-    def history(self):
-        return pd.DataFrame(self._history)
-
-    def handle_update(self, value):
-        super(History, self).handle_update(value)
-        self._history.append(value)
-
-
-# TODO replace with a list of sub stopping conditions with an or or an and
-class StoppingCondition(DifferenceStoppingCondition,
-                        IterationStoppingCondition):
-
-    def __init__(self, **kwargs):
-        super(StoppingCondition, self).__init__(**kwargs)
-
-    def handle_update(self, value):
-        DifferenceStoppingCondition.handle_update(self, value)
-        IterationStoppingCondition.handle_update(self, value)
-
-    def should_stop(self):
-        return IterationStoppingCondition.should_stop(self) or\
-            DifferenceStoppingCondition.should_stop(self)
+from . import functional
 
 
 def sample_expectation(samples, g):
     """
-
     Parameters
     ----------
     samples : pandas.DataFrame
-    g : callable
-
+    g : Callable<float(pandas.Series)> or
+        Callable<pandas.Series(pandas.Series)>
     Returns
     -------
-    float
+    float or pandas.Series
     """
     sample_values = samples.apply(g, axis=1)
     return sample_values.mean()
 
 
-def fast_ica(samples, f, f_dot, stopping_condition=None):
+def fast_ica(samples, f, f_dot, **kwargs):
     """
 
     Parameters
     ----------
     samples
-    f
-    f_dot
-    stopping_condition : IStoppingCondition or None
+    f: Callable<float(float)>
+    f_dot: Callable<float(float)>
+        Derivative of the above algorithm
+    kwargs: dict
+        Passed onto functional.iteration_algorithm
 
     Returns
     -------
 
     """
 
-    stopping_condition = stopping_condition or StoppingCondition()
+    def _a(w):
+        return sample_expectation(samples, lambda x: f_dot(np.dot(w, x))) * w
 
-    def _a(u):
-        return sample_expectation(samples, lambda x: f_dot(np.dot(u, x))) * u
+    def _b(w):
+        return sample_expectation(samples, lambda x: f(np.dot(w, x)) * x)
 
-    def _b(u):
-        return sample_expectation(samples, lambda x: f(np.dot(u, x)) * x)
-
-    w = pd.Series([not i for i in range(len(samples.columns))])
-
-    while not stopping_condition.should_stop():
+    def update(w):
         w = _a(w) - _b(w)
         w = w / np.sqrt(np.dot(w, w))
-
         if isinstance(w, pd.Series) and w[0] < 0.0:
             w = -1.0 * w
+        return w
 
-        stopping_condition.handle_update(w)
+    w0 = pd.Series([not i for i in range(len(samples.columns))])
 
-    return w, stopping_condition
+    return functional.iteration_algorithm(update, w0, **kwargs)
